3f885e10a4a83816296cb9985e0b2490d473a29e
JayS <jrschwane@uwalumni.com>
2016-05-15 18:45:00 -0500

    Working on filters...

--- js/app.js
+++ js/app.js
@@ -13,8 +13,10 @@
                 return $http.get(url).then(function (data) {
                     return Papa.parse(data.data, {
                         header: true,
+                        dynamicTyping: true,
+                        skipEmptyLines: true,
                         complete: function (results) {
-                            // console.log('Papaparse data = ' + results.data);   // for testing
+                            // console.log('Papaparse results: ' + results.data);   // for testing
                             return results.data;
                         }
                     });
@@ -30,13 +32,38 @@
     // ============================= CONTROLLERS ==============================
     app.controller('csvParseController', function ($scope, testFactory) {
 
-        testFactory.getStuff().then(function (data) {
+        testFactory.getStuff().then(function (results) {
             
-            // console.log('Factory data = ' + data);   // for testing
+            var i,
+                formattedResults = [];
             
-            $scope.outputData = data.data;
+            $scope.minDateSlider = 0;
+            $scope.maxDateSlider = 100;
+            
+            // convert parsed date strings to JS dates (seconds)
+            for (i = 0; i < results.data.length; i += 1) {
+                formattedResults.push({
+                    source : results.data[i].source,
+                    target : results.data[i].target,
+                    value  : results.data[i].value,
+                    annum  : Date.parse(results.data[i].annum)
+                });
+            }
+            
+            // bind formatted data to $scope object
+            $scope.outputData = formattedResults;
+            
+            // find date min/max
+            $scope.minDate = Math.min.apply(Math, formattedResults.map(function (objMin) {
+                return objMin.annum;
+            }));
+
+            $scope.maxDate = Math.max.apply(Math, formattedResults.map(function (objMax) {
+                return objMax.annum;
+            }));
             
         });
+        
     });
 
 
