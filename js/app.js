363056a9e574ab46723a9bae68a832075084fa46
JayS <jrschwane@uwalumni.com>
2016-05-15 16:39:35 -0500

    Got PapaParse working ... no idea what the problem was, prob some typo. Sheesh

--- js/app.js
+++ js/app.js
@@ -1,62 +1,45 @@
 (function () {
     'use strict';
 
-    angular
-        
-        .module('csvParse', [])
-    
-        .value()
-    
-        .controller('csvParseController', ['$http', function ($http) {
+    var app = angular.module('csvParse', []);
+
+
+    // =============================== SERVICES ===============================
+    app.factory('testFactory', ['$http', '$q', function ($http, $q) {
+
+        var url = 'data/data.csv',
             
-            var vm = this,
-                results,
-                outputData;
-            
-            vm.minDate;
-            vm.maxDate;
-            vm.outputData = [];
-            
-            // work with the data
-            function doStuff(data) {
-                // Data is usable here
-                vm.outputData = data;
-                console.log('Callback\'s outputData: ' + vm.outputData);
-                console.log('Callback\'s outputData type: ' + typeof vm.outputData);
-            }
-            
-            // read in the CSV
-            function parseData(url, callBack) {
-                Papa.parse(url, {
-                    download: true,
-                    header: true,
-                    dynamicTyping: true,
-                    skipEmptyLines: true,
-                    complete: function(results) {
-                        callBack(results.data);
-                    }
+            getStuff = function () {
+                return $http.get(url).then(function (data) {
+                    return Papa.parse(data.data, {
+                        header: true,
+                        complete: function (results) {
+                            // console.log('Papaparse data = ' + results.data);   // for testing
+                            return results.data;
+                        }
+                    });
                 });
-            }
+            };
+
+        return {
+            getStuff: getStuff
+        };
+    }]);
+    
+
+    // ============================= CONTROLLERS ==============================
+    app.controller('csvParseController', function ($scope, testFactory) {
+
+        testFactory.getStuff().then(function (data) {
             
-            parseData('data/data.csv', doStuff);
-
-            console.log('Type of outputData = ' + typeof outputData);
-            console.log('outputData: ' + outputData);
- //           console.log('Length of outputData: ' + outputData.length);
+            // console.log('Factory data = ' + data);   // for testing
             
-            console.log('Type of vm.outputData = ' + typeof vm.outputData);
-            console.log('outputData: ' + vm.outputData);
-            console.log('Length of outputData: ' + vm.outputData.length);
+            $scope.outputData = data.data;
+            
+        });
+    });
 
 
-            // begin find date ranges                
-//            vm.minDate = Math.min.apply(Math, vm.parsedData.map(function (objMin) {
-//                return objMin.annum;
-//            }));
-//
-//            vm.maxDate = Math.max.apply(Math, vm.parsedData.map(function (obj) {
-//                return obj.annum;
-//            }));
 
-        }]);   // end controller
+    
 })();
